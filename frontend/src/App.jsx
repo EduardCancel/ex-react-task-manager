a4967785a73489ecc0e0df4cdc7fa5e411d5b2e8
IlTuoNome <laTuaEmail@example.com>
2025-07-17 17:37:43 +0200

    Complete Milestone 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Tasklist from './pages/TaskList';
 import AddTask from './pages/AddTask';
+import { GlobalProvider } from './context/GlobalContext';
 import './index.css';
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <nav>
-        < NavLink to="/">Task List</NavLink>
-        < NavLink to="/add">Add Task</NavLink>
-      </nav>
-      <Routes>
-        < Route path="/" element={< Tasklist />} />
-        < Route path="/add" element={< AddTask />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+
+        <nav>
+          < NavLink to="/">Task List</NavLink>
+          < NavLink to="/add">Add Task</NavLink>
+        </nav>
+
+        <Routes>
+          < Route path="/" element={< Tasklist />} />
+          < Route path="/add" element={< AddTask />} />
+        </Routes>
+
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -1,4 +1,12 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+
+
 export default function TaskList() {
+
+    const { tasks } = useContext(GlobalContext);
+    console.log('tasks:', tasks);
+
     return (
         <div>
             <h1>Task List</h1>
