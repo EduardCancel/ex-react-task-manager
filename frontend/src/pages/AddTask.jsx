0cb2bc7d6b12265eab1931d045e102f5c75f18ad
IlTuoNome <laTuaEmail@example.com>
2025-07-17 13:17:40 +0200

    Implement Milestone 1

--- frontend/src/pages/AddTask.jsx
+++ frontend/src/pages/AddTask.jsx
@@ -2,11 +2,7 @@ export default function AddTask() {
     return (
         <div>
             <h1>Add Task</h1>
-            <form>
-                <input type="text" placeholder="Task Title" />
-                <textarea placeholder="Task Description"></textarea>
-                <button type="submit">Add Task</button>
-            </form>
+            <p>Inizio milestone 1</p>
         </div>
     );
 }
